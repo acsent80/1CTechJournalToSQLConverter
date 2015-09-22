6ad495c1077c2de9f6442a7cbdef69653f9c28cd
acsent80 <acsent@list.ru>
2015-09-22 18:47:31 +0300

    first commit

--- src/com/acsent/Main.java
+++ src/com/acsent/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
 
-        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("resourses/main.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();

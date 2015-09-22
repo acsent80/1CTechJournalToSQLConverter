50864b229f8d50998702f63a1a5136f3b78f8e8f
acsent80 <acsent@list.ru>
2015-09-22 18:45:54 +0300

    first commit

---
+++ src/com/acsent/Controller.java
@@ -0,0 +1,7 @@
+package com.acsent;
+
+/**
+ * Created by msk5446 on 22.09.2015.
+ */
+public class Controller {
+}
--- src/com/acsent/Main.java
+++ src/com/acsent/Main.java
@@ -1,11 +1,25 @@
 package com.acsent;
 
-/**
- * Created by msk5446 on 22.09.2015.
- */
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+
+        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
 
     public static void main(String[] args) {
-        System.out.println(1);
+        launch(args);
     }
 }
+

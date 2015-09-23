7a9f709d0c5c0c58c74c4eb28f846161f943f266
acsent <acsent@list.ru>
2015-09-23 10:44:10 +0300

    choose dir

--- src/com/acsent/mainController.java
+++ src/com/acsent/mainController.java
@@ -1,7 +1,27 @@
 package com.acsent;
 
-/**
- * Created by msk5446 on 22.09.2015.
- */
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+import javax.swing.*;
+import java.io.File;
+
 public class mainController {
+
+    @FXML
+    javafx.scene.control.Label dirLabel;
+    @FXML
+    javafx.scene.control.TextField  dirText;
+
+    public void dirButtonOnAction(ActionEvent actionEvent) {
+
+        JFileChooser chooser = new JFileChooser();
+        chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        int result = chooser.showOpenDialog(null);
+        if (result == JFileChooser.APPROVE_OPTION) {
+            File selectedFile = chooser.getSelectedFile();
+            dirText.setText(selectedFile.getPath());
+        }
+
+    }
 }
